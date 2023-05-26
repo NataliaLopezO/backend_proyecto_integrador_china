e71c39d13bbf0e33b3bad823035f22e4ab275224
NataliaLopezO <natalia.lopez.osorio@correounivalle.edu.co>
2023-05-26 14:40:38 -0500

    cors allowed all2

--- china/settings.py
+++ china/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'china.wsgi.application'
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -131,7 +132,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [ '*' ]
+
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
